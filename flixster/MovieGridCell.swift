1bb24c4ffbaa462a85bb9d4955b1dfc2d9146cbe
Dipu Chowdhury <71580429+Dipu-Chowdhury@users.noreply.github.com>
2021-07-19 02:35:37 -0400

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Muna Chowdhury on 7/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
